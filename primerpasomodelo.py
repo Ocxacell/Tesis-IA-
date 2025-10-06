64a232d7a06361386477f22c9aa364592ebe91e4
ocxacell <lemussebastian843@gmail.com>
2025-10-06 11:21:31 -0500

    Se separaron los features en numericos y categoricos

--- primerpasomodelo.py
+++ primerpasomodelo.py
@@ -5,11 +5,12 @@ from catboost import CatBoostClassifier, Pool
 # 1. Leer el archivo limpio
 df = pd.read_excel("Datosrecorte_limpio.xlsx", sheet_name="Data")
 
-# 2. Definir la variable target (ejemplo: 'Masivo', cámbiala si es otra)
+# 2. Definir la variable target y columnas a ignorar(ejemplo: 'Masivo', cámbiala si es otra)
 target = "tiempo_solucion_horas"
+rows_to_ignore = ["Fecha_Solucion", target]  # Añade aquí las columnas que no quieres usar
 
 # 3. Features = todas las demás columnas excepto target
-X = df.drop(columns=[target])
+X = df.drop(columns=rows_to_ignore)
 y = df[target]
 
 # 4. Dividir en train y test
@@ -19,3 +20,11 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 print("Tamaño train:", X_train.shape)
 print("Tamaño test:", X_test.shape)
+print(X.head())   
+print(y.head())   
+# 1️⃣ Identificar columnas categóricas y numéricas
+cat_features = X_train.select_dtypes(include=["object"]).columns.tolist()
+num_features = X_train.select_dtypes(exclude=["object"]).columns.tolist()
+
+print("Categorical features:", cat_features)
+print("Numeric features:", num_features)
